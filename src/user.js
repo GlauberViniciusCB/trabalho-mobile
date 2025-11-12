fed76da8f8ea60b68a5dbdb1337f8ae1c065eeea
Natan822 <133471092+Natan822@users.noreply.github.com>
2025-11-12 13:33:20 -0300

    feat(backend): move User class to separate file

---
+++ src/user.js
@@ -0,0 +1,50 @@
+import Peer from 'peerjs'
+
+export default class User {
+
+	constructor() {
+		this.peer = new Peer();
+		this.conn = null;
+		this.id = "";
+		this.is_connected = false;
+		this.is_peer_created = false;
+
+		this.peer.on('open', (_id) => {
+			this.id = _id;
+			this.is_peer_created = true;
+			console.log(`Peer criado com ID: ${_id}`);
+		})
+	}
+
+	connect(destId) {
+		if (!this.is_peer_created) {
+			console.error("Tentativa de conexão antes do peer ser criado");
+			return;
+		}
+
+		this.conn = this.peer.connect(destId);
+		this.conn.on('open', () => {
+			this.is_connected = true;
+
+			this.conn.on('data', (data) => {
+				this.receiveDataCallback(data);
+			});
+
+			console.log(`Conexão estabelecida com ID: ${destId}`);
+		});
+	}
+
+	receiveDataCallback(data) {
+		// TODO: tratar recebimento de mensagens/dados
+		console.log(`Mensagem recebida: ${data}`);
+	}
+
+	sendMessage(msg) {
+		if (!this.is_connected) {
+			console.error("Tentativa de enviar mensagem antes de fazer conexão.");
+			return;
+		}
+		this.conn.send(msg);
+	}
+
+}
