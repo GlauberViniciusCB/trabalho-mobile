7a2a3918dd399fa60edf68e6a70add759161d037
Natan822 <133471092+Natan822@users.noreply.github.com>
2025-12-10 15:49:40 -0300

    feat(videoplayer): marcação de tempo

--- src/video_player.js
+++ src/video_player.js
@@ -1,6 +1,6 @@
 class VideoPlayer {
 
-    constructor(uploadBtnId, videoInputId, videoPlayerId, mediaControlsId, togglePlayBtnId, videoPlayerDivId, pauseIconId, playIconId) {
+    constructor(uploadBtnId, videoInputId, videoPlayerId, mediaControlsId, togglePlayBtnId, videoPlayerDivId, pauseIconId, playIconId, currentTimeId, durationId) {
         this.uploadBtn = document.getElementById(uploadBtnId);
         this.input = document.getElementById(videoInputId);
         this.player = document.getElementById(videoPlayerId);
@@ -9,6 +9,8 @@ class VideoPlayer {
         this.playerDiv = document.getElementById(videoPlayerDivId);
         this.playIcon = document.getElementById(playIconId);
         this.pauseIcon = document.getElementById(pauseIconId);
+        this.currentTime = document.getElementById(currentTimeId);
+        this.duration = document.getElementById(durationId);
 
         this.isVideoPlaying = false;
 
@@ -71,6 +73,33 @@ class VideoPlayer {
 
         this.playIcon.classList.add('hidden');
         this.pauseIcon.classList.remove('hidden');
+        this.updateCurrentTime();
+    }
+
+    updateCurrentTime() {
+        if (!this.isVideoPlaying) {
+            return;
+        }
+
+        let currentTimeFormatted = VideoPlayer.formatTime(this.player.currentTime);
+        this.currentTime.textContent = currentTimeFormatted;
+
+        let timeout = (1.0 - (this.player.currentTime % 1)) * 1000;
+        setTimeout(() => {
+            this.updateCurrentTime();
+        }, timeout);
+    }
+
+    static formatTime(seconds) {
+        const totalSeconds = Math.floor(seconds);
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const secs = totalSeconds % 60;
+
+        let formattedTime = hours == 0 ? '' : String(hours).padStart(2, '0') + ':';
+        return formattedTime +
+            String(minutes).padStart(2, '0') + ':' +
+            String(secs).padStart(2, '0');
     }
 
     startVideo(media) {
@@ -79,8 +108,14 @@ class VideoPlayer {
 
         this.player.src = media;
         this.player.style.display = "block";
+
+        this.player.addEventListener('loadedmetadata', () => {
+            let durationTime = VideoPlayer.formatTime(this.player.duration);
+            this.duration.textContent = durationTime;
+        });
+
         this.play();
     }
 }
 
-const videoPlayer = new VideoPlayer('uploadBtn', 'videoInput', 'videoPlayer', 'mediaControls', 'togglePlayBtn', 'videoPlayerDiv', 'pauseIcon', 'playIcon');
+const videoPlayer = new VideoPlayer('uploadBtn', 'videoInput', 'videoPlayer', 'mediaControls', 'togglePlayBtn', 'videoPlayerDiv', 'pauseIcon', 'playIcon', 'currentTime', 'duration');
