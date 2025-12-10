49b9a9a5ea080c0e0a6fc4d4466add0ff1ff90cd
Natan822 <133471092+Natan822@users.noreply.github.com>
2025-12-10 14:24:07 -0300

    feat: botao de play/pause

--- src/video_player.js
+++ src/video_player.js
@@ -1,38 +1,86 @@
 class VideoPlayer {
-    
-    constructor(uploadBtnId, videoInputId, videoPlayerId) {
+
+    constructor(uploadBtnId, videoInputId, videoPlayerId, mediaControlsId, togglePlayBtnId, videoPlayerDivId, pauseIconId, playIconId) {
         this.uploadBtn = document.getElementById(uploadBtnId);
-        this.videoInput = document.getElementById(videoInputId);
-        this.videoPlayer = document.getElementById(videoPlayerId);
+        this.input = document.getElementById(videoInputId);
+        this.player = document.getElementById(videoPlayerId);
+        this.mediaControls = document.getElementById(mediaControlsId);
+        this.togglePlayBtn = document.getElementById(togglePlayBtnId);
+        this.playerDiv = document.getElementById(videoPlayerDivId);
+        this.playIcon = document.getElementById(playIconId);
+        this.pauseIcon = document.getElementById(pauseIconId);
+
+        this.isVideoPlaying = false;
 
         this.init();
     }
 
     init() {
         this.uploadBtn.addEventListener('click', () => this.handleUploadClick());
-        this.videoInput.addEventListener('change', (e) => this.handleFileChange(e));
+        this.input.addEventListener('change', (e) => this.handleFileChange(e));
+        this.playerDiv.addEventListener('mouseenter', (e) => this.handleMouseEnter(e));
+        this.playerDiv.addEventListener('mouseleave', (e) => this.handleMouseLeave(e));
+        this.togglePlayBtn.addEventListener('click', (e) => this.handleTogglePlayBtn(e));
     }
 
     handleUploadClick() {
-        this.videoInput.click();
+        this.input.click();
     }
 
     handleFileChange(e) {
-        const file = this.videoInput.files[0];
+        const file = this.input.files[0];
         console.log(`[DEBUG]: Arquivo recebido: "${file.name}"`);
 
         const media = URL.createObjectURL(file);
-        this.playVideo(media);
+        this.startVideo(media);
     }
 
-    playVideo(media) {
+    handleMouseEnter(e) {
+        if (this.isVideoPlaying) {
+            this.mediaControls.classList.remove('hidden');
+        }
+    }
+
+    handleMouseLeave(e) {
+        if (this.isVideoPlaying) {
+            this.mediaControls.classList.add('hidden');
+        }
+    }
+
+    handleTogglePlayBtn(e) {
+        e.preventDefault();
+        if (this.isVideoPlaying) {
+            this.pause();
+        }
+        else {
+            this.play();
+        }
+    }
+
+    pause(e) {
+        this.player.pause();
+        this.isVideoPlaying = false;
+
+        this.pauseIcon.classList.add('hidden');
+        this.playIcon.classList.remove('hidden');
+    }
+
+    play(e) {
+        this.player.play();
+        this.isVideoPlaying = true;
+
+        this.playIcon.classList.add('hidden');
+        this.pauseIcon.classList.remove('hidden');
+    }
+
+    startVideo(media) {
         document.querySelector('.videoPlayer').classList.remove('barulho');
         document.querySelector('.uploadBtn').classList.add('hidden');
 
-        this.videoPlayer.src = media;
-        this.videoPlayer.style.display = "block";
-        this.videoPlayer.play();
+        this.player.src = media;
+        this.player.style.display = "block";
+        this.play();
     }
 }
 
-const videoPlayer = new VideoPlayer('uploadBtn', 'videoInput', 'videoPlayer');
+const videoPlayer = new VideoPlayer('uploadBtn', 'videoInput', 'videoPlayer', 'mediaControls', 'togglePlayBtn', 'videoPlayerDiv', 'pauseIcon', 'playIcon');
